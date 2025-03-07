f18f70eabfc9584818946dd2dbd28fec85df9ecd
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-07 18:42:55 -0500

    Añadiendo mejoras de errores

--- src/main/scala/ejercicio6.scala
+++ src/main/scala/ejercicio6.scala
@@ -1,10 +1,11 @@
 import Paises._
 
 enum Paises:
-  case MX,USA,CU,GD,AR,None
+  case MX,USA,CU,GD,AR
 end Paises
 
-object paises:
+object Paises:
+
   def isPais(pais_seleccionado: Paises): String = pais_seleccionado match
     case MX => "Este país es México"
     case USA => "Este país es Estados Unidos"
@@ -14,18 +15,19 @@ object paises:
     case _ => "País no seleccionado"
   end isPais
 
-  def fromName(name: String): Any = name.toUpperCase() match
-    case "MX" => Paises.MX
-    case "USA" => Paises.USA
-    case "CU" => Paises.CU
-    case "GD" => Paises.GD
-    case "AR" => Paises.AR
-    case _ => "País no seleccionado"
+  def fromName(name: String): Paises =
+    name.toUpperCase() match
+      case "MX" => Paises.MX
+      case "USA" => Paises.USA
+      case "CU" => Paises.CU
+      case "GD" => Paises.GD
+      case "AR" => Paises.AR
   end fromName
-end paises
-
+end Paises
 
 
 @main def run : Unit =
-  println(paises.isPais(MX))
-
+  try
+    println(Paises.fromName("PR"))
+  catch
+    case e: Exception => println("País no encontrado")
