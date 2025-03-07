8a1805d7765259f562347e0f230c02c9286cbe40
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-07 16:05:09 -0500

    Ejercicios sobre match y primer ejercicio del trabajo

--- src/main/scala/ejercicio5.scala
+++ src/main/scala/ejercicio5.scala
@@ -5,4 +5,10 @@ end UnitedKingdom
 import UnitedKingdom._
 def malditosEscoceses(pais:UnitedKingdom = Scotland) = if pais.equals(Scotland) then "Malditos escoceses han arruinado Escocia" else "No te llevaste el chiste"
 
+def reescrituraEspañol(pais:UnitedKingdom) = pais match
+  case UnitedKingdom.England => "Por el rey y la reina"
+  case UnitedKingdom.NothernIreland => "Somos mejor que el sur"
+  case UnitedKingdom.Scotland => "Malditos escoceses han arruinado Escocia"
+  case UnitedKingdom.Wales => "El país de Gareth Bale"
+
 @main def chistoSimpsons: Unit = println(malditosEscoceses(England))
