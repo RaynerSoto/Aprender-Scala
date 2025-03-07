c91904cefca9f635ee66f2293e2a2ccff884b71b
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-07 16:06:21 -0500

    Ejercicios del trabajo 1

--- src/main/scala/caseClaseEnums/Evento.scala
+++ src/main/scala/caseClaseEnums/Evento.scala
@@ -3,14 +3,14 @@ package caseClaseEnums
 import Pais._
 
 def posicion_final(jugadores: Array[Jugador]): Array[Jugador] =
-  var posicion = 0
   jugadores.sortBy(s => s.tiempo)
 
 // Para clasificar a las Olimpiadas son los 56 primeros atletas en pasa
 @main def evento: Unit = {
   val jugadores = Array(
     Jugador("Rayner Alejandro Soto Martínez", 24,Pais.CU, 16.23),
-    Jugador("Dianelis de las Mercedes Estenoz Vazquez", 23,Pais.MX, 22.31)
+    Jugador("Dianelis de las Mercedes Estenoz Vazquez", 23,Pais.MX, 22.31),
+    Jugador("Usain Bolt",40,Pais.USA,9.19)
   )
   posicion_final(jugadores).take(56).foreach(s => println(s.jugadorMessage))
 }
---
+++ src/main/scala/ejercicio6.scala
@@ -0,0 +1,31 @@
+import Paises._
+
+enum Paises:
+  case MX,USA,CU,GD,AR,None
+end Paises
+
+object paises:
+  def isPais(pais_seleccionado: Paises): String = pais_seleccionado match
+    case MX => "Este país es México"
+    case USA => "Este país es Estados Unidos"
+    case CU => "Este país es Cuba"
+    case GD => "Este país es Granada"
+    case AR => "Este país es Argentina"
+    case _ => "País no seleccionado"
+  end isPais
+
+  def fromName(name: String): Any = name match
+    case "MX" => Paises.MX
+    case "USA" => Paises.USA
+    case "CU" => Paises.CU
+    case "GD" => Paises.GD
+    case "AR" => Paises.AR
+    case _ => "País no seleccionado"
+  end fromName
+end paises
+
+
+
+@main def run : Unit =
+  println(paises.isPais(MX))
+
