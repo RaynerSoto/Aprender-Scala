9a6d390f547c352c7f1733292c3a8b05e8bead7a
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-07 18:52:57 -0500

    Mejoras en el código del ejercicio 6

--- src/main/scala/ejercicio6.scala
+++ src/main/scala/ejercicio6.scala
@@ -15,19 +15,21 @@ object Paises:
     case _ => "País no seleccionado"
   end isPais
 
-  def fromName(name: String): Paises =
-    name.toUpperCase() match
-      case "MX" => Paises.MX
-      case "USA" => Paises.USA
-      case "CU" => Paises.CU
-      case "GD" => Paises.GD
-      case "AR" => Paises.AR
+  def fromName(name: String): Option[Paises] =
+    try
+      Some(name.toUpperCase() match
+        case "MX" => Paises.MX
+        case "USA" => Paises.USA
+        case "CU" => Paises.CU
+        case "GD" => Paises.GD
+        case "AR" => Paises.AR)
+    catch
+      case  e:Exception => None
+
   end fromName
 end Paises
 
 
-@main def run : Unit =
-  try
-    println(Paises.fromName("PR"))
-  catch
-    case e: Exception => println("País no encontrado")
+@main def run : Unit = Paises.fromName("Peru") match
+  case Some(value) => println(value)
+  case None => println("No se encontró el país")
