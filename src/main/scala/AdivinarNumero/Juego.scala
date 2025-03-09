e4897ee5c995a5f1eb1aba5c751860beaa21dc64
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-09 19:54:48 -0400

    Añadiendo correcciones a los ejercicios para el trabajo con el Long y para la interfaz

--- src/main/scala/AdivinarNumero/Juego.scala
+++ src/main/scala/AdivinarNumero/Juego.scala
@@ -5,12 +5,12 @@ import java.util.{Date, Scanner}
 import scala.util.Random
 
 @main def interfaz_principal : Unit =
-  println("Indique que número desea que el ordenador le adivine")
+  println("Indique que número desea que el ordenador le adivine en el rango de: -999999999999999999 y 999999999999999999L")
   val s = new Scanner(System.in).nextLong()
   val inicio = new Date().getTime
   println(adivinar(valor = s) + s" con un tiempo de ${new Date().getTime - inicio} milisegundos")
 
-def adivinar(minimo: Long = -1000000000, maximo:Long = 1000000000, valor: Long, iteraciones: Long = 0): String =
+def adivinar(minimo: Long = -999999999999999999L, maximo:Long = 999999999999999999L, valor: Long, iteraciones: Long = 0): String =
   val numero = Random.between(minimo,maximo)
   if (numero == valor) then s"El número a sido encontrado con ${iteraciones} iteraciones"
   else if numero < valor then adivinar(numero,maximo, valor,iteraciones + 1)
