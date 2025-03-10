8760f597e6316c39d8fc81d0884f1a0b04e092a7
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-10 12:33:52 -0400

    Ejercicios sobre recursión con fibonacci

--- src/main/scala/course2/ejercicio9.scala
+++ src/main/scala/course2/ejercicio9.scala
@@ -6,6 +6,15 @@ def concatenar(valor:String, repeticion: Int = 1): String = {
   else valor + concatenar(valor,repeticion-1)
 }
 
+//Fibonnaci con validación para valores negativos
+def fibonacci(valor1:Long = 0, valor2:Long = 1, posicion:Long, iterador: Long = 3): Long = {
+  posicion match
+    case 1 => valor1
+    case 2 => valor2
+    case posicion if posicion < 0 => -1
+    case _ =>  if iterador == posicion then valor1+valor2 else fibonacci(valor2,valor1 + valor2,posicion,iterador + 1)
+}
+
 @main def ejercicio9():Unit = {
-  println(concatenar("Scala",2))
+  println(fibonacci(posicion = 15))
 }
