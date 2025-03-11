55a2f4f077d7b759f4d01ecb889d5213294be483
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-11 16:23:02 -0400

    Mejorando el método abrir maletero

--- src/main/scala/course2/ejercicio13/Coche.scala
+++ src/main/scala/course2/ejercicio13/Coche.scala
@@ -1,7 +1,8 @@
 package course2.ejercicio13
 
-class Coche(modelo:String,matricula:String,propietario:String,velocidad_max:Double)  extends Vehiculo(modelo,matricula,propietario,velocidad_max){
+class Coche(modelo:String,matricula:String,propietario:String,velocidad_max:Double,maletero:Boolean)  extends Vehiculo(modelo,matricula,propietario,velocidad_max){
   def abrirMaletero(valor:Boolean): String = valor match
-    case true => "Maletero abierto"
+    case valor if valor && maletero => "Maletero abierto"
+    case valor if !maletero => "El auto no tiene maletero"
     case _=> "Maletero cerrado"
 }
