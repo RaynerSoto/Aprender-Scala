b81cf3b25aefac4467cc4176f2ecec2f525c165b
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-13 19:44:37 -0400

    Haciendo arreglos en el ejercicio 13

--- src/main/scala/course2/ejercicio13/Vehiculo.scala
+++ src/main/scala/course2/ejercicio13/Vehiculo.scala
@@ -1,19 +1,21 @@
 package course2.ejercicio13
 
 abstract class Vehiculo(modelo:String,matricula:String,propietario:String,velocidad_max:Double) {
-  private var velocidadActual = 0
+  private var velocidadActual:Double = 0
   def mostrarInformacion: String = s"Modelo del auto: $modelo, con matricula: $matricula, cuyo propetario es: $propietario y que alcanza un máximo de velocidad de: $velocidad_max"
   def acelerar(valor:Double): Either[String,String] = {
     velocidadActual + valor match
       case x if velocidadActual + valor <= velocidad_max =>
-        Right(this.velocidadActual += valor)
+       this.velocidadActual += valor
+        Right(s"Velocidada disminuida a $this.velocidadActual")
       case _ => Left("No se puede aumentar más la velocidad")
   }
   def frenar(valor:Double): Either[String,String] = {
     velocidadActual - valor match
       case x if velocidadActual - valor >= 0 =>
-        Right(this.velocidadActual -= valor)
+        velocidadActual -= valor
+        Right(s"Velocidada disminuida a $valor")
       case _ => Left("No se puede disminuir más la velocidad al frenar")
   }
-  def detener:Long = this.velocidadActual = 0
+  def detener:Unit = this.velocidadActual = 0D
 }
---
+++ src/main/scala/course2/ejercicio14.scala
@@ -0,0 +1,8 @@
+package course2
+
+// De una lista de números imprime la cantidad la cantidad de scaladel número
+
+
+@main def ejercio14(args: Array[String]): Unit = {
+
+}
