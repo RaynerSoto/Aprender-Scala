5c05a34ebb128dff3c24c604d41b930564cb769d
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-03-13 20:42:44 -0400

    Ejercicios sobre listas

--- src/main/scala/course2/ejercicio14.scala
+++ src/main/scala/course2/ejercicio14.scala
@@ -1,8 +1,27 @@
 package course2
 
-// De una lista de números imprime la cantidad la cantidad de scaladel número
+/*
+ De una lista de números imprime  la cantidad de scala del número
+ Por ejemplo 1 - Scala, 2 - ScalaScala
+ */
+def imprimir(valor:List[Int]): List[String] = {
+  valor.flatMap(s => List(List.fill(s)("Scala").mkString))
+}
 
+/*
+ Generar los 100 primero números a través de una lista de manera recurviva
+*/
+def numerosRecursivos(valor:Int = 1): List[Int] = {
+  if valor == 100 then valor::Nil else valor::numerosRecursivos(valor+1)
+}
 
-@main def ejercio14(args: Array[String]): Unit = {
+/*
+ De los 100 primeros números cuales son primos o no
+*/
+def numerosPrimos(): List[Int] = {
+  numerosRecursivos().filter(isPrimo(_))
+}
 
+@main def ejercio14(): Unit = {
+  println(numerosPrimos())
 }
